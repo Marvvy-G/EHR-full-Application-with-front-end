e96f7dd4323ab2dfdcc628fea88f8925942111e0
Etukudoh Marvellous <Marvellous Etukudoh>
2022-09-15 00:05:22 +0100

    work in progress

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ dotenv.config();
 
 //Database connection
 //change database and deploy
-// mongoose.connect("mongodb+srv://bellsehr:password1234@bellsehr.bwuj4eh.mongodb.net/?retryWrites=true&w=majority");
-mongoose.connect("mongodb://localhost/authdemonew");
+mongoose.connect("mongodb+srv://bellsehr:password1234@bellsehr.bwuj4eh.mongodb.net/?retryWrites=true&w=majority");
+// mongoose.connect("mongodb://localhost/authdemonew");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
